2491dafe87b437c3f7578e6cefb6442fb0ed6ab4
张莹光 <yingguang@staff.sina.com.cn>
2021-01-21 11:22:30 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h1>黑马头条内容发布系统</h1>
+    <!-- 路由出口 -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less"></style>
---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+// 路由配置表
+const routes = []
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
